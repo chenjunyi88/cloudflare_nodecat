40068a667d1c294d3d799def0a01734419f17edd
chenjunyi88 <61383008+chenjunyi88@users.noreply.github.com>
2022-04-20 01:09:30 +0800

    Update scan1.py

--- scan1.py
+++ scan1.py
@@ -40,7 +40,7 @@ class tThread(threading.Thread):
 
 def checkalive():
 	print('\n[Step1] Scanning alive servers:\n')
-	zmap = os.popen('zmap -interface 207.2.123.65 -w ' + SETIPLIST + ' -p 443 -B 30M')
+	zmap = os.popen('zmap -interface venet0 -w ' + SETIPLIST + ' -p 443 -B 30M')
 	global IPLIST
 	IPLIST = zmap.read().splitlines()
 
